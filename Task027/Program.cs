69ad3c8da5f6db67bc7b5ab1dc92e12554a97518
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-11-08 18:29:23 +0300

    Task27

--- Task027/Program.cs
+++ Task027/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// Например: 452 -> 11; 82 -> 10; 9012 -> 12
+
+int SumDigit (int number)
+{
+    int sum = default;
+    int i = number % 10;
+    while(i > 0)
+    {
+        sum = sum + i;
+        number = number / 10;
+        i = number % 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите целое число:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n >= 0)
+{
+int s = SumDigit(n);
+//int s = SumDigit(Math.Abs(n));
+Console.WriteLine($"Сумма цифр числа {n} составляет {s}");
+}
+else
+{
+Console.WriteLine("Введите положительное число");
+}
