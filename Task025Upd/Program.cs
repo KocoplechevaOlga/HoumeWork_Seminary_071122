29a76a113f94a9b43658f2e054f63c548fec29bf
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-11-08 19:48:32 +0300

    Task25 Universal solution

--- Task025Upd/Program.cs
+++ Task025Upd/Program.cs
@@ -1,31 +1,34 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-int PowNumber (int n, int p)
+double PowNumber (double n, double p)
 {
-    int res = n;
-    if (p == 0 && n != 0) res = 1;
-    else
-    {
-    for(int i = 2; i <= p && i > 1; i++)
+    double res = n;
+    for(double i = 2; i <= p && i > 1; i++)
     {
        res = res*n;
-    }
     }
      return res;
 }
 
 Console.WriteLine("Введите число, возводимое в степень:");
-int a = Convert.ToInt32(Console.ReadLine());
+double a = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите степень числа:");
-int b = Convert.ToInt32(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
 if (b >= 0)
 {
-int number = PowNumber(a, b);
-Console.WriteLine($"Число {a} в степень {b} составляет {number}");
+   if (b == 0 && a != 0)
+   {
+      Console.WriteLine($"Число {a} в степень {b} составляет 1");
+   }
+   else
+   {
+      double number1 = PowNumber(a, b);
+      Console.WriteLine($"Число {a} в степень {b} составляет {number1}");
+   }
 }
 else 
 {
-int c = Math.Abs(b);
-int number = PowNumber(a, c);
-Console.WriteLine($"Число {a} в натуральной степени {b} составляет {number}");
+double c = Math.Abs(b);
+double number2 = 1 / (PowNumber(a, c));
+Console.WriteLine($"Число {a} в степени {b} составляет {number2}");
 }
