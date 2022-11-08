641e06e62bbb07ebc9d9957ffc2e8f6297994472
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-11-08 16:46:15 +0300

    Task25

--- Task025/Program.cs
+++ Task025/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int PowNumber (int n, int p)
+{
+    int res = n;
+    if (p == 1) res = res*1;
+    if (p == 0 && n != 0) res = 1;
+    else
+    {
+    for(int i = 2; i <= p; i++)
+    {
+       res = res*n;
+    }
+    }
+     return res;
+}
+
+Console.WriteLine("Введите число, возводимое в степень:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень числа:");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b >= 0)
+{
+int number = PowNumber(a, b);
+Console.WriteLine($"Число {a} в степень {b} составляет {number}");
+}
+else 
+{
+    Console.WriteLine("Степень числа должна быть натуральным чилом, введете целое положительное число");
+}
