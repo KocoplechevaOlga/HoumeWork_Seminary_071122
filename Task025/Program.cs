c8b84831618f9dea5fa3592a92ca643cc8cb9b46
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-11-08 19:30:26 +0300

    Task25 upd2

--- Task025/Program.cs
+++ Task025/Program.cs
@@ -3,13 +3,9 @@
 int PowNumber (int n, int p)
 {
     int res = n;
-    if (p == 0 && n != 0) res = 1;
-    else
-    {
     for(int i = 2; i <= p && p > 1; i++)
     {
        res = res*n;
-    }
     }
      return res;
 }
@@ -18,12 +14,12 @@ Console.WriteLine("Введите число, возводимое в степе
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите степень числа:");
 int b = Convert.ToInt32(Console.ReadLine());
-if (b >= 0)
+if (b > 0)
 {
 int number = PowNumber(a, b);
 Console.WriteLine($"Число {a} в степень {b} составляет {number}");
 }
-else 
+else
 {
     Console.WriteLine("Степень числа должна быть натуральным чилом, введете целое положительное число");
 }
